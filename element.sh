0206b00fc8bbac844983a68e7b2ec4e9963124b2
Aleksei Spolysov <perematom@gmail.com>
2024-12-27 10:05:34 +0000

    feat: added if/else clause for selection in element.sh

--- element.sh
+++ element.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-
-echo Please provide an element as an argument.
-
+if [[ ! -z $1 ]]
+then
+  case $1 in
+  # if $1 is a string, then I need to run check on element symbol and name
+  ''|*[!0-9]*) echo String ;;
+  # if $1 is a number, then I need to run select on atomic_number in elements table
+  *)  echo Number;;
+  esac
+else
+  echo Please provide an element as an argument.
+fi
